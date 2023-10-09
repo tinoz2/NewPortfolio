b12cbc47f62037088c2eb564ea4e387e599b7c3e
Santino <mellamoalroberto@gmail.com>
2023-10-09 07:00:23 -0300

    changeRedirects

--- Portfolio/src/Container.jsx
+++ Portfolio/src/Container.jsx
@@ -4,6 +4,7 @@ import Skills from "./components/Skills"
 import Projects from "./components/Projects"
 import MoreProjects from "./components/MoreProjects"
 import FooterContact from "./components/FooterContact"
+import Certificates from "./components/Certificates"
 import './css/navbar.css'
 import './css/style.css'
 import './css/main.css'
@@ -11,6 +12,7 @@ import './css/skills.css'
 import './css/projects.css'
 import './css/moreprojects.css'
 import './css/footercontact.css'
+import './css/certificates.css'
 
 const Container = () => {
     return (
@@ -20,6 +22,7 @@ const Container = () => {
             <Skills />
             <Projects />
             <MoreProjects />
+            <Certificates />
             <FooterContact />
         </>
     )
---
+++ Portfolio/src/components/Certificates.jsx
@@ -0,0 +1,22 @@
+import certificateWEB from '../img/diploma.png'
+import certificateJS from '../img/certificado2.png'
+import imgLink from '../img/link.svg'
+
+const Certificates = () => {
+    return (
+        <>
+            <div>
+                <h3 className='h3-skills h3-certificates'><span className='span-skills'>My </span>Certificates:</h3>
+                <div className='container-img-certificate'>
+                    <img className='img-certificate' src={certificateWEB} alt="CODERHOUSE CERTIFICATE" />
+                    <img className='img-certificate' src={certificateJS} alt="CODERHOUSE CERTIFICATE" />
+                </div>
+                <div className='container-button-certificate'>
+                    <a target='blank' href="https://www.coderhouse.com/"><img className='icon-moreprojects' src={imgLink} alt="CODERHOUSE CERTIFICATE LINK" /></a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Certificates
